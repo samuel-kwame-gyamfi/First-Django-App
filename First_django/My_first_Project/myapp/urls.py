bc3efdc625de62df0361242c90522d62688573ab
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-03-03 23:31:25 +0000

    git views and urls

--- First_django/My_first_Project/myapp/urls.py
+++ First_django/My_first_Project/myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("greetings",views.hello)
+    # path("greetings",views.hello)
+    path("<day>",views.hello)
 ]
--- First_django/My_first_Project/myapp/views.py
+++ First_django/My_first_Project/myapp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 # Create your views here.
 
-def hello(request):
-    return HttpResponse ("<h1>Hello World !!!</h1>")
+def hello (request, day):
+    learn = None
+    if day == "sunday":
+        learn = "<h1>Learn Data Structures and Web Concept </h1>"
+    elif day == "Monday":
+        learn = "<h1>Learn HCI and Operations Research</h1>"
+    elif day == "wednesday":
+        learn ="<h1>It's a free day</h1>"
+    elif day == "thursday":
+        learn = "<h1>learn financial accounting</h1>"
+    else:
+        return HttpResponseNotFound("<h1>yayyyyyyy it's weekends!!!</h1>")
+    return HttpResponse(learn)
+
+    
